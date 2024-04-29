05c3093cded12b507a8d3078a886691a3b0fdfc4
Md Ashfaque alam <myofficeashfaque@gmail.com>
2024-04-29 18:44:07 +0300

    modified:   README.md 	modified:   config/config.yaml 	modified:   params.yaml 	new file:   research/01_data_ingestion.ipynb 	modified:   schema.yaml 	modified:   src/mlProject/constant/__init__.py 	modified:   src/mlProject/utils/commom.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # End-to-end-Machine-Learning-project-with-MLflow
+
+update config.yaml
+update schema.yaml
+update params.yaml
+update the entity
+update the configuratiion manager in src config
+update the components
+update the pipeline
+update the main.py
+update the app.py
--- src/mlProject/utils/commom.py
+++ src/mlProject/utils/commom.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import Logger
+from src.mlProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -26,7 +26,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            Logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
@@ -47,7 +47,7 @@ def create_directories(path_to_directories: list,verbose = True):
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            Logger.info(f"created directory at: {path}")
+            logger.info(f"created directory at: {path}")
             
 
 @ensure_annotations
@@ -62,7 +62,7 @@ def save_json(path: Path, data: dict):
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
         
-    Logger.info(f"json file saved at: {path}")
+    logger.info(f"json file saved at: {path}")
     
 
 @ensure_annotations
@@ -78,7 +78,7 @@ def load_json(path: Path) -> ConfigBox:
     with open(path) as f:
         content = json.load(f)
         
-    Logger.info(f"json file loaded successfully from: {path}") 
+    logger.info(f"json file loaded successfully from: {path}") 
     return ConfigBox(content)  
 
 
@@ -93,7 +93,7 @@ def save_bin(data: Any, path: Path):
     """
     
     joblib.dump(value=data,filename=path)
-    Logger.info(f"binary file saved at: {path}")
+    logger.info(f"binary file saved at: {path}")
     
     
 @ensure_annotations
@@ -106,7 +106,7 @@ def load_bin(path: Path) -> Any:
         Any: object stored in the file
     """
     data = joblib.load(path)
-    Logger.info(f"binary file loaded from: {path}")
+    logger.info(f"binary file loaded from: {path}")
     return data
 
 
