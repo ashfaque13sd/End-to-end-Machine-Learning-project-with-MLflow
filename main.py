0d89aac7fa8e5b9c8651f08c2a91d12285852e7c
Md Ashfaque alam <myofficeashfaque@gmail.com>
2024-04-29 20:23:26 +0300

    modified:   main.py 	modified:   research/01_data_ingestion.ipynb 	modified:   research/trials.ipynb 	new file:   src/mlProject/components/data_ingestion.py 	modified:   src/mlProject/config/configuration.py 	modified:   src/mlProject/entity/config_entity.py 	new file:   src/mlProject/pipeline/stage_01_data_ingestion.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from src.mlProject import logger
-logger.info("welcome to our custom logging")
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/mlProject/config/configuration.py
@@ -0,0 +1,30 @@
+from src.mlProject.constant import *
+from src.mlProject.utils.commom import read_yaml, create_directories
+from src.mlProject.entity.config_entity import DataIngestionConfig
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_flepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
+        
+        self.config = read_yaml(config_filepath) 
+        self.params = read_yaml(params_flepath)
+        self.schema = read_yaml(schema_filepath)
+        
+        create_directories([self.config.artifacts_root])
+        
+        
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+        
+        create_directories([config.root_dir])
+        
+        data_ingestion_config = DataIngestionConfig(
+            root_dir = config.root_dir, 
+            source_URL = config.source_URL,
+            local_data_file = config.local_data_file, 
+            unzip_dir = config.unzip_dir
+        ) 
+        return data_ingestion_config
