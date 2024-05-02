5244fc1411b8c247c62238a63a7e01c1faf1c54e
Md Ashfaque alam <myofficeashfaque@gmail.com>
2024-05-02 18:57:05 +0300

    model evaluation and mlflow added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from src.mlProject.pipeline.stage_02_data_validation import DataValidationTraini
 from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
+from src.mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -52,3 +54,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = ModelEvaluationTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
